239f71e54086187d551eafb57fd92add01b8b99f
dobutamine <tim.antonius@outlook.com>
2021-05-20 12:19:09 +0200

    update

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 
 import random
 
-
 app = FastAPI()
 
 origins = [
@@ -36,46 +35,40 @@ class ReqIdModel(BaseModel):
 
 class DataModel(BaseModel):
     id: str
-    hr: float
-    sat_pre: float
-    sat_post: float
-    resp_rate: float
+    heartrate: float
+    satPre: float
+    satPost: float
+    satVen: float
+    respRate: float
     etco2: float
+    abpSyst: float
+    abpDiast: float
     pfi: float
     temp: float
-    abp_syst: float
-    abp_diast: float
+    cvp: float
+    papSyst: float
+    papDiast: float
+    imageNo: float
+    resusState: float
+    rhythmType: float
+    rhythmParameter: float
+    curveSqueeze: float
+    hrEnabled: bool
+    satPreEnabled: bool
+    satPostEnabled: bool
+    satVenEnabled: bool
+    abpEnabled: bool
+    respRateEnabeld: bool
+    etco2Enabled: bool
+    tempEnabled: bool
+    polsEnabled: bool
+    pfiEnabled: bool
+    nibdEnabled: bool
+    cvpEnabled: bool
+    papEnabled: bool
+    alarmEnabled: bool
 
-
-default_response_object = {
-    'error': '',
-    'id': '',
-    'hr': 120,
-    'sat_pre': 98,
-    'sat_post': 95,
-    'resp_rate': 30,
-    'etco2': 4.5,
-    'pfi': 1.5,
-    'temp': 37.6,
-    'abp_syst': 70,
-    'abp_diast': 50
-}
-
-emulators = {
-    'YODA': {
-        'error': '',
-        'id': '2412',
-        'hr': 120,
-        'sat_pre': 98,
-        'sat_post': 95,
-        'resp_rate': 30,
-        'etco2': 4.5,
-        'pfi': 1.5,
-        'temp': 37.6,
-        'abp_syst': 70,
-        'abp_diast': 50
-    }   
-}
+emulators = {}
 
 @app.post("/removeid")
 async def removeId(reqId: ReqIdModel):
@@ -88,20 +81,7 @@ async def removeId(reqId: ReqIdModel):
 
 @app.post("/regid")
 async def regId(regId: ReqIdModel):
-    newObject = {
-        'error': '',
-        'id': regId.id,
-        'hr': 150,
-        'sat_pre': 100,
-        'sat_post': 100,
-        'resp_rate': 35,
-        'etco2': 5.0,
-        'pfi': 1.1,
-        'temp': 37.2,
-        'abp_syst': 75,
-        'abp_diast': 5
-    }
-    emulators[regId.id] = newObject
+    emulators[regId.id] =  DataModel()
     print("registrating new id: ", regId.id)
     return {"OK"}
 
@@ -109,7 +89,6 @@ async def regId(regId: ReqIdModel):
 async def getdata(reqId: ReqIdModel):
     print("requesting data from id: ", reqId.id)
     if reqId.id in emulators.keys():
-        print(emulators[reqId.id])
         return emulators[reqId.id]
     else:
         print("requesting data not found for id: ", reqId.id)
@@ -118,22 +97,8 @@ async def getdata(reqId: ReqIdModel):
 @app.post("/setdata")
 async def setdata(newdata: DataModel):
     if newdata.id in emulators.keys():
-        newObject = {
-            'error': '',
-            'id': newdata.id,
-            'hr': newdata.hr,
-            'sat_pre': newdata.sat_pre,
-            'sat_post': newdata.sat_post,
-            'resp_rate': newdata.resp_rate,
-            'etco2': newdata.etco2,
-            'pfi': newdata.pfi,
-            'temp': newdata.temp,
-            'abp_syst': newdata.abp_syst,
-            'abp_diast': newdata.abp_diast
-        }
-        emulators[newdata.id] = newObject
-        print("new data set for id: ", newObject['id'])
-        return {"new data set"}
+        emulators[newdata.id] = newdata
+        return {"OK"}
     else:
         print("error setting new data for id: ", newdata.id)
         return { "error": "id not found"}
